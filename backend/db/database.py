6724ecba188e43d5c3aa3021aa666c43f7b5708c
Dhany Tama <33150516+DhanyTama@users.noreply.github.com>
2025-09-24 19:19:52 +0700

    fix: env local

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
+load_dotenv(".env.local")
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
